aa6387c4a9773ca332417f586775fc2791c2168b
Monisha R <monishar729735@gmail.com>
2024-10-12 16:41:54 +0530

    handle signin login using navigate and toast

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,12 +1,11 @@
 from dj_rest_auth.registration.views import RegisterView
-from dj_rest_auth.views import LoginView, LogoutView #, UserDetailsView
+from dj_rest_auth.views import LoginView, LogoutView, UserDetailsView
 from django.urls import path
-from .views import user_detail_view
 
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="rest_register"),
     path("login/", LoginView.as_view(), name="rest_login"),
     path("logout/", LogoutView.as_view(), name="rest_logout"),
-    path('user/', user_detail_view, name='user-detail'),
+    path('user/', UserDetailsView.as_view(), name='user-detail'),
 ]
--- frontend/src/pages/LoginSignUpPage.jsx
+++ frontend/src/pages/LoginSignUpPage.jsx
@@ -1,25 +1,67 @@
 import React, { useState } from "react";
 import * as Components from './Components.js';
+import axios from "axios"; // Axios for API calls
+import { toast } from 'react-toastify';
+import {useNavigate} from 'react-router-dom';
 
 function LoginSignUpPage() {
     const [signIn, toggle] = useState(true);
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [error, setError] = useState(null);
+    const navigate = useNavigate();
 
-    const handleSignUpSubmit = (e) => {
+    const handleRegister = async (e) => {
         e.preventDefault();
-        // Send a POST request to the Django API with the form data
-        
-    }
 
-    const handleSignInSubmit = (e) => {
+        if (password !== confirmPassword) {
+        setError("Passwords do not match");
+        return;
+        }
+
+        try {
+        const response = await axios.post("http://localhost:8000/users/register/", {
+            email: email,
+            password1: password,  // Password1 for dj-rest-auth
+            password2: confirmPassword  // Password2 for confirmation
+        });
+        console.log(response.data); // Handle success, e.g. navigate to login page
+        toggle(true);
+        toast.success('Created account successfully, login with your credentials to continue.')
+
+        } catch (error) {
+        console.error(error.response.data); // Handle error
+        setError(error.response.data);
+        }
+    };
+
+    const handleLogin = async (e) => {
         e.preventDefault();
-    }
+        try {
+          const response = await axios.post("http://localhost:8000/users/login/", {
+            email: email,
+            password: password
+          });
+          console.log(response.data); // Handle successful login
+          navigate('/');
+          toast.success('You have logged in successfully.')
+        } catch (error) {
+            // Check if error.response exists
+            if (error.response) {
+              console.error(error.response.data); // Handle error response
+              toast.error('Login failed. Please check your credentials.');
+            } else {
+              console.error('Error:', error.message); // Handle any other error (network, etc.)
+              toast.error('An error occurred. Please try again later.');
+            }
+          }
+      };
 
     return (
         <Components.Container>
             <Components.SignUpContainer signinIn={signIn}>
-                <Components.Form onSubmit={handleSignUpSubmit}>
+                <Components.Form onSubmit={handleRegister}>
                     <Components.Title>Create Account</Components.Title>
                     <Components.Input type='text' 
                                       placeholder='Email' 
@@ -28,13 +70,14 @@ function LoginSignUpPage() {
                                       placeholder='Password' 
                                       onChange={(e) => setPassword(e.target.value)} />
                     <Components.Input type='password'
-                                      placeholder='Confirm Password' />
+                                      placeholder='Confirm Password'
+                                      onChange={(e) => setConfirmPassword(e.target.value)} />
                     <Components.Button>Sign Up</Components.Button>
                 </Components.Form>
             </Components.SignUpContainer>
 
             <Components.SignInContainer signinIn={signIn}>
-                <Components.Form onSubmit={handleSignInSubmit}>
+                <Components.Form onSubmit={handleLogin}>
                     <Components.Title>Sign in</Components.Title>
                     <Components.Input type='text'
                                       placeholder='Email'
