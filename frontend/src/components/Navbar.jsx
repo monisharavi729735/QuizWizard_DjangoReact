4b8328003221847cbe3bb841461749cb37e369fa
Monisha R <monishar729735@gmail.com>
2024-10-30 17:08:16 +0530

    login signup completed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,95 +1,89 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import wizard from '../assets/images/wizard.png'
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import wizard from '../assets/images/wizard.png';
 
-const Navbar = () => { 
-
+const Navbar = () => {
   const [user, setUser] = useState(null);
+  const navigate = useNavigate();
 
   useEffect(() => {
-      const fetchUserData = async () => {
-          const token = localStorage.getItem('authToken'); // Check for the token
-          if (token) {
-              try {
-                  const response = await axios.get('http://127.0.0.1:8000/api/auth/user/', {
-                      headers: {
-                          'Content-Type': 'application/json',
-                          'Authorization': `Token ${token}`,
-                      }
-                  });
-                  setUser(response.data);
-              } catch (error) {
-                  setUser(null);
-              }
-          } else {
-              setUser(null); // No token found, set user to null
-          }
-      };
+    const fetchUserData = async () => {
+      const token = localStorage.getItem('authToken');
+      if (token) {
+        try {
+          const response = await axios.get('http://127.0.0.1:8000/api/auth/user/', {
+            headers: {
+              'Authorization': `Token ${token}`,
+            },
+          });
+          setUser(response.data);
+        } catch (error) {
+          console.error("Failed to fetch user data:", error.response);
+          setUser(null);
+        }
+      } else {
+        setUser(null);
+      }
+    };
 
-      fetchUserData();
+    fetchUserData();
   }, []);
 
-  const linkClass = ({isActive}) => isActive ? 
-  "text-white bg-black hover:bg-gray-900 hover:text-white rounded-md px-3 py-2" : 
-  "text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2"
+  const handleLogout = () => {
+    localStorage.removeItem('authToken');  // Remove token from localStorage
+    setUser(null);  // Clear user state
+    navigate('/');  // Redirect to home or login page
+  };
+
+  const linkClass = "text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2";
+  const activeLinkClass = "text-white bg-black";
 
   return (
-    <>
-      <nav className="bg-cyan-700 border-b border-cyan-500">
+    <nav className="bg-cyan-700 border-b border-cyan-500">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="flex h-20 items-center justify-between">
-          <div
-            className="flex flex-1 items-center justify-center md:items-stretch md:justify-start"
-          >
-            {/* <!-- Logo --> */}
+          <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
             <NavLink className="flex flex-shrink-0 items-center mr-4" to="/">
-              <img
-                className="h-10 w-auto"
-                src={wizard}
-                alt="QuizWizard"
-              />
-              <span className="hidden md:block text-white text-2xl font-bold ml-2"
-                >QuizWizard</span
-              >
+              <img className="h-10 w-auto" src={wizard} alt="QuizWizard" />
+              <span className="hidden md:block text-white text-2xl font-bold ml-2">QuizWizard</span>
             </NavLink>
             <div className="md:ml-auto">
               <div className="flex space-x-2">
-                <NavLink
-                  to="/"
-                  className={linkClass}
-                  >Home</NavLink
-                >
-                <NavLink
-                  to="/quizzes"
-                  className={linkClass}
-                  >Quizzes</NavLink
-                >
-                <NavLink
-                  to="/add-quiz"
-                  className={linkClass}
-                  >New Quiz</NavLink
-                >
-
-                <NavLink
-                  to="/auth"
-                  className={linkClass}
-                  >Login</NavLink
-                >
-                <div className={linkClass}>
-                  {user 
-                      ? <p className="text-white bg-cyan-800 rounded-md px-3 py-2 ml-6">{user.email}</p>
-                      : <p className="text-white bg-cyan-800 rounded-md px-3 py-2 ml-6">Guest</p>
-                  }
-              </div>
+                {user ? (
+                  <div className="text-white bg-cyan-800 rounded-md px-3 py-2 ml-6">{user.email}</div>
+                ) : (
+                  <div className="text-white bg-cyan-800 rounded-md px-3 py-2 ml-6">Guest</div>
+                )}
+                <NavLink 
+                  to="/" 
+                  className={({ isActive }) => isActive ? `${linkClass} ${activeLinkClass}` : linkClass}>
+                  Home
+                </NavLink>
+                <NavLink 
+                  to="/quizzes" 
+                  className={({ isActive }) => isActive ? `${linkClass} ${activeLinkClass}` : linkClass}>
+                  Quizzes
+                </NavLink>
+                <NavLink 
+                  to="/add-quiz" 
+                  className={({ isActive }) => isActive ? `${linkClass} ${activeLinkClass}` : linkClass}>
+                  New Quiz
+                </NavLink>
+                
+                {/* Conditional rendering for Login and Logout */}
+                {user ? (
+                  <button onClick={handleLogout} className={linkClass}>Logout</button>
+                ) : (
+                  <NavLink to="/auth" className={linkClass}>Login</NavLink>
+                )}
               </div> 
             </div>
-            </div>
-            </div>
           </div>
+        </div>
+      </div>
     </nav>
-    </>
   );
 };
 
-export default Navbar
+export default Navbar;
--- frontend/src/pages/LoginSignUpPage.jsx
+++ frontend/src/pages/LoginSignUpPage.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import * as Components from './Components.js';
-import axios from "axios"; // Axios for API calls
+import axios from "axios";
 import { toast } from 'react-toastify';
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 function LoginSignUpPage() {
   const [signIn, toggle] = useState(true);
@@ -10,142 +10,131 @@ function LoginSignUpPage() {
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
   const [error, setError] = useState(null);
-  const [user, setUser] = useState(null);
   const navigate = useNavigate();
   
   const handleRegister = async (e) => {
-      e.preventDefault();
-      setError(null); // Reset any previous error
-      if (password !== confirmPassword) {
-          toast.error("Passwords do not match.");
-          return;
-      }
-      try {
-          const response = await axios.post("http://localhost:8000/api/auth/register/", {
-              email: email,
-              password1: password,
-              password2: confirmPassword,
+    e.preventDefault();
+    setError(null);
+
+    if (password !== confirmPassword) {
+      toast.error("Passwords do not match.");
+      return;
+    }
+    
+    try {
+      const response = await axios.post("http://localhost:8000/api/auth/register/", {
+        email,
+        password1: password,
+        password2: confirmPassword,
+      });
+      console.log(response.data);
+      navigate('/');
+      toast.success('Registration successful!');
+    } catch (error) {
+      if (error.response) {
+        const errors = error.response.data;
+        if (errors.password1) {
+          errors.password1.forEach(err => {
+            if (err.includes("too short")) toast.error("Password must be at least 8 characters.");
+            if (err.includes("too common")) toast.error("Choose a more secure password.");
           });
-          console.log(response.data); // Log successful registration response
-          navigate('/');
-          toast.success('Registration successful!');
-      } catch (error) {
-          if (error.response) {
-              const errors = error.response.data;
-              
-              if (errors.password1) {
-                  errors.password1.forEach(err => {
-                      if (err.includes("This password is too short")) {
-                          toast.error("Password is too short, it must contain at least 8 characters.");
-                      }
-                      if (err.includes("This password is too common")) {
-                          toast.error("Password is too common, please choose a more secure one.");
-                      }
-                      if (err.includes("Passwords do not match")) {
-                          toast.error("Passwords do not match.");
-                      }
-                  });
-              }
-  
-              if (errors.email) {
-                  toast.error("Email is already in use. Please choose a different one.");
-              }
-              
-          } else {
-              console.error('Error:', error.message); // Handle other errors (network, etc.)
-              toast.error('An error occurred. Please try again later.');
-          }
-      }
-  };
-  
-  const handleLogin = async (e) => {
-      e.preventDefault();
-      setError(null); // Reset any previous error
-      try {
-          const response = await axios.post("http://localhost:8000/api/auth/login/", {
-              email: email,
-              password: password
-          });
-          console.log(response.data); // Log successful login response
-          const token = response.data.key;  // or response.data.token depending on the backend response
-          localStorage.setItem("authToken", token); // Store token in localStorage
-          navigate('/');
-          toast.success('You have logged in successfully.');
-          if (response.status === 200) {
-            // Handle successful login, e.g., store token
-            localStorage.setItem('authToken', response.data.token);
-            setUser(response.data.user); // Update user state if necessary
         }
-      } catch (error) {
-          if (error.response) {
-              console.error(error.response.data); // Log error response
-              toast.error('Login failed. Please check your credentials.');
-          } else {
-              console.error('Error:', error.message); // Handle any other error (network, etc.)
-              toast.error('An error occurred. Please try again later.');
-          }
+        if (errors.email) toast.error("Email is already in use.");
+      } else {
+        toast.error('An error occurred. Please try again later.');
       }
+    }
   };
 
-    return (
-        <Components.Container>
-            <Components.SignUpContainer signinIn={signIn}>
-                <Components.Form onSubmit={handleRegister}>
-                    <Components.Title>Create Account</Components.Title>
-                    <Components.Input type='text' 
-                                      placeholder='Email' 
-                                      onChange={(e) => setEmail(e.target.value)} />
-                    <Components.Input type='password'
-                                      placeholder='Password' 
-                                      onChange={(e) => setPassword(e.target.value)} />
-                    <Components.Input type='password'
-                                      placeholder='Confirm Password'
-                                      onChange={(e) => setConfirmPassword(e.target.value)} />
-                    <Components.Button>Sign Up</Components.Button>
-                </Components.Form>
-            </Components.SignUpContainer>
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    setError(null);
 
-            <Components.SignInContainer signinIn={signIn}>
-                <Components.Form onSubmit={handleLogin}>
-                    <Components.Title>Sign in</Components.Title>
-                    <Components.Input type='text'
-                                      placeholder='Email'
-                                      value={email}
-                                      onChange={(e) => setEmail(e.target.value)}/>
-                    <Components.Input type='password'
-                                      placeholder='Password'
-                                      value={password}
-                                      onChange={(e) => setPassword(e.target.value)}/>
-                    {/* <Components.Anchor href='#'>Forgot your password?</Components.Anchor> */}
-                    <Components.Button>Sign In</Components.Button>
-                </Components.Form>
-            </Components.SignInContainer>
+    try {
+      const response = await axios.post("http://localhost:8000/api/auth/login/", {
+        email,
+        password
+      });
+      const token = response.data.key;
+      localStorage.setItem("authToken", token);
+      window.location.href = '/';
+      toast.success('You have logged in successfully.');
+    } catch (error) {
+      if (error.response) {
+        toast.error('Login failed. Check your credentials.');
+      } else {
+        toast.error('An error occurred. Please try again later.');
+      }
+    }
+  };
 
-            <Components.OverlayContainer signinIn={signIn}>
-                <Components.Overlay signinIn={signIn}>
-                    <Components.LeftOverlayPanel signinIn={signIn}>
-                        <Components.Title>Welcome Back!</Components.Title>
-                        <Components.Paragraph>
-                            Please login with your personal info
-                        </Components.Paragraph>
-                        <Components.GhostButton onClick={() => toggle(true)}>
-                            Sign In
-                        </Components.GhostButton>
-                    </Components.LeftOverlayPanel>
+  return (
+    <Components.Container>
+      <Components.SignUpContainer signinIn={signIn}>
+        <Components.Form onSubmit={handleRegister}>
+          <Components.Title>Create Account</Components.Title>
+          <Components.Input 
+            type='text' 
+            placeholder='Email' 
+            onChange={(e) => setEmail(e.target.value)} 
+          />
+          <Components.Input 
+            type='password'
+            placeholder='Password' 
+            onChange={(e) => setPassword(e.target.value)} 
+          />
+          <Components.Input 
+            type='password'
+            placeholder='Confirm Password'
+            onChange={(e) => setConfirmPassword(e.target.value)} 
+          />
+          <Components.Button>Sign Up</Components.Button>
+        </Components.Form>
+      </Components.SignUpContainer>
 
-                    <Components.RightOverlayPanel signinIn={signIn}>
-                        <Components.Title>First time here?</Components.Title>
-                        <Components.Paragraph>
-                            Please create an account to proceed further
-                        </Components.Paragraph>
-                        <Components.GhostButton onClick={() => toggle(false)}>
-                            Sign Up
-                        </Components.GhostButton>
-                    </Components.RightOverlayPanel>
-                </Components.Overlay>
-            </Components.OverlayContainer>
-        </Components.Container>
-    );
+      <Components.SignInContainer signinIn={signIn}>
+        <Components.Form onSubmit={handleLogin}>
+          <Components.Title>Sign in</Components.Title>
+          <Components.Input 
+            type='text'
+            placeholder='Email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Components.Input 
+            type='password'
+            placeholder='Password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <Components.Button>Sign In</Components.Button>
+        </Components.Form>
+      </Components.SignInContainer>
+
+      <Components.OverlayContainer signinIn={signIn}>
+        <Components.Overlay signinIn={signIn}>
+          <Components.LeftOverlayPanel signinIn={signIn}>
+            <Components.Title>Welcome Back!</Components.Title>
+            <Components.Paragraph>
+              Please login with your personal info
+            </Components.Paragraph>
+            <Components.GhostButton onClick={() => toggle(true)}>
+              Sign In
+            </Components.GhostButton>
+          </Components.LeftOverlayPanel>
+          <Components.RightOverlayPanel signinIn={signIn}>
+            <Components.Title>First time here?</Components.Title>
+            <Components.Paragraph>
+              Please create an account to proceed further
+            </Components.Paragraph>
+            <Components.GhostButton onClick={() => toggle(false)}>
+              Sign Up
+            </Components.GhostButton>
+          </Components.RightOverlayPanel>
+        </Components.Overlay>
+      </Components.OverlayContainer>
+    </Components.Container>
+  );
 }
 
 export default LoginSignUpPage;
